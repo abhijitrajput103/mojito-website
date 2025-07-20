72c08f7f924a18c702d271551c7ba5c12e33e336
abhijeetrajput103 <abhijeetrajput103@gmail.com>
2025-07-20 14:34:43 +0530

    `Added Contact page and made CSS adjustments`

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Art from "./components/Art";
 import Cocktails from "./components/Cocktails";
+import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Menu from "./components/Menu";
 
@@ -12,6 +13,7 @@ export default function Home() {
       <About/>
       <Art/>
       <Menu/>
+      <Contact/>
     </main>
   );
 }
